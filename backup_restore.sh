c0cebc03dea292e84f13eb17acb3e639f8995693
elkhayyat <ahmed.elkhayyat@onlink4it.com>
2023-02-02 21:22:22 +0200

    add restore_remote, pull_updates features

--- backup_restore.sh
+++ backup_restore.sh
@@ -75,6 +75,65 @@ function restore_postgres() {
     psql -U "$pg_user" "$db_name" < "postgres_backups/$file_name"
 }
 
+# Function to restore backup from remote server
+function restore_remote() {
+    # Ask for remote server information
+    read -p "Enter remote server address: " server
+    read -p "Enter username: " username
+    read -p "Enter the path to the backup on the remote server: " backup_path
+
+    # Check if the server can be accessed using key or password or preconfigured ssh configs
+    echo "Checking access to remote server..."
+    ssh -q $username@$server exit
+    if [ $? -ne 0 ]; then
+        # If access failed, ask for password
+        read -sp "Password: " password
+        echo
+        scp -r $username:$backup_path .
+    else
+        scp -r $username@$server:$backup_path .
+    fi
+
+    # Ask for database information
+    read -p "Enter database name: " database
+    read -p "Enter database username: " db_username
+    read -sp "Enter database password: " db_password
+    echo
+
+    # Check if database is MySQL or PostgreSQL
+    read -p "Enter database type (mysql or postgres): " db_type
+    if [ "$db_type" == "mysql" ]; then
+        # Restore MySQL database
+        echo "Restoring MySQL database..."
+        mysql -u $db_username -p$db_password $database < $backup_path
+    elif [ "$db_type" == "postgres" ]; then
+        # Restore PostgreSQL database
+        echo "Restoring PostgreSQL database..."
+        psql -U $db_username -d $database -f $backup_path
+    else
+        echo "Invalid database type, please try again."
+    fi
+}
+
+
+# Function to pull updates from Git repository
+function pull_updates() {
+    # Check if the current directory is a Git repository
+    if [ ! -d ".git" ]; then
+        echo "Error: Not a Git repository."
+        return
+    fi
+
+    # Fetch the latest changes from the remote repository
+    git fetch origin
+
+    # Checkout the specified branch
+    git checkout main
+
+    # Pull the latest changes for the specified branch
+    git pull origin main
+}
+
 # Function to display menu
 function display_menu() {
     echo "===================="
@@ -123,6 +182,7 @@ function process_selection() {
 
 # Continuously display menu and process user selection until exit
 while true; do
+    pull_updates
     display_menu
     get_selection
     process_selection
